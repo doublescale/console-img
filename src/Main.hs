3f17ccdf63331864a375a42c04a6d44cb2bfcf9e
doublescale <doublescale@tutanota.com>
2018-02-23 00:14:09 +0100

    implement

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,46 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
 
 import Codec.Picture
 import Data.List
-import GHC.Word
+import System.Environment
+import System.Exit
 
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  case args of
+    [fileName] -> putImageFile fileName
+    _ -> do
+      putStrLn "Give me one picture file as an argument."
+      exitFailure
 
-data RGB888 = RGB888 Word8 Word8 Word8 deriving (Show)
+putImageFile :: FilePath -> IO ()
+putImageFile fileName = do
+  eitherImage <- readImage fileName
+  case eitherImage of
+    Right dynImage -> putStr (renderRgbMatrix (imageToMatrix dynImage))
+    Left errString -> do
+      putStrLn errString
+      exitFailure
 
-ansiColoredHalfBlocks :: RGB888 -> RGB888 -> String
-ansiColoredHalfBlocks topCol botCol =
-  "\ESC[48;2;" ++ triple topCol ++ ";38;2;" ++ triple botCol ++ "m\9604\ESC[0m"
+imageToMatrix :: DynamicImage -> [[PixelRGB8]]
+imageToMatrix dynImage =
+  [[pixelAt img x y | x <- [0 .. imageWidth - 1]] | y <- [0 .. imageHeight - 1]]
   where
-    triple (RGB888 r g b) = intercalate ";" (map show [r, g, b])
+    img@Image {imageHeight, imageWidth} = convertRGB8 dynImage
 
-renderRgbMatrix :: [[RGB888]] -> String
+renderRgbMatrix :: [[PixelRGB8]] -> String
 renderRgbMatrix =
   unlines . map concat . transpose . map colorPairwise . transpose
   where
     colorPairwise (a:b:xs) = [ansiColoredHalfBlocks a b] ++ colorPairwise xs
-    colorPairwise [a] = [ansiColoredHalfBlocks a (RGB888 0 0 0)]
+    colorPairwise [a] = [ansiColoredHalfBlocks a (PixelRGB8 0 0 0)]
     colorPairwise [] = []
+
+ansiColoredHalfBlocks :: PixelRGB8 -> PixelRGB8 -> String
+ansiColoredHalfBlocks topCol botCol =
+  "\ESC[48;2;" ++ triple topCol ++ ";38;2;" ++ triple botCol ++ "m\9604\ESC[0m"
+  where
+    triple (PixelRGB8 r g b) = intercalate ";" (map show [r, g, b])
